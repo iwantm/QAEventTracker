b31e767883a2eaeca9bc08e6ae354a278a186e6f
Iwan Moreton <me@iwanmoreton.com>
2020-11-13 10:33:32 +0000

    remove creation

--- tests/test_int.py
+++ tests/test_int.py
@@ -38,30 +38,30 @@ class TestBase(LiveServerTestCase):
         self.driver.get("http://localhost:5000")
         db.drop_all()
         db.create_all()
-        new_user1 = Users(user_name='user1',
-                          email='user1@user.com',
-                          password=bcrypt.generate_password_hash('password'))
-        new_user2 = Users(user_name='user2',
-                          email='user2@user.com',
-                          password=bcrypt.generate_password_hash('password2'))
-        new_event1 = Events(title='Event1',
-                            description='desc1',
-                            date=datetime(2020, 12, 13, 00, 00, 00))
-        new_event2 = Events(title='Event2',
-                            description='desc2',
-                            date=datetime(2020, 12, 13, 00, 00, 00))
-        db.session.add(new_user1)
-        db.session.add(new_user2)
-        db.session.add(new_event1)
-        db.session.add(new_event2)
-        db.session.commit()
-        new_group1 = Groups(user_id=new_user1.id,
-                            event_id=new_event1.id)
-        new_group2 = Groups(user_id=new_user2.id,
-                            event_id=new_event2.id)
-        db.session.add(new_group1)
-        db.session.add(new_group2)
-        db.session.commit()
+        # new_user1 = Users(user_name='user1',
+        #                   email='user1@user.com',
+        #                   password=bcrypt.generate_password_hash('password'))
+        # new_user2 = Users(user_name='user2',
+        #                   email='user2@user.com',
+        #                   password=bcrypt.generate_password_hash('password2'))
+        # new_event1 = Events(title='Event1',
+        #                     description='desc1',
+        #                     date=datetime(2020, 12, 13, 00, 00, 00))
+        # new_event2 = Events(title='Event2',
+        #                     description='desc2',
+        #                     date=datetime(2020, 12, 13, 00, 00, 00))
+        # db.session.add(new_user1)
+        # db.session.add(new_user2)
+        # db.session.add(new_event1)
+        # db.session.add(new_event2)
+        # db.session.commit()
+        # new_group1 = Groups(user_id=new_user1.id,
+        #                     event_id=new_event1.id)
+        # new_group2 = Groups(user_id=new_user2.id,
+        #                     event_id=new_event2.id)
+        # db.session.add(new_group1)
+        # db.session.add(new_group2)
+        # db.session.commit()
 
     def tearDown(self):
         self.driver.quit()
